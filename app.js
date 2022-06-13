e0b3da95f72bfa7dbb4813bd84139483382df382
Alyson <98368053+alysonmccomas@users.noreply.github.com>
2022-06-12 21:49:18 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('It works!');
+function show() {
+  document.getElementById('image')
+                    .style.display = "block";
+
+            document.getElementById('btnID')
+                    .style.display = "none";
+}
